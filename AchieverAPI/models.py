5ff42ef46d97ca63ef391ac218b64f3f3aa482b8
Ian McLerran <imclerran@protonmail.com>
2024-04-15 11:15:19 -0500

    Added AchieverAPI app, and associated models

---
+++ AchieverAPI/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    isDone = models.BooleanField(default=False)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    difficulty = models.SmallIntegerField(default=1)
+    due_date = models.DateTimeField(null=True, blank=True)
+    done_date = models.DateTimeField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Subtask(models.Model):
+    isDone = models.BooleanField(default=False)
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    difficulty = models.SmallIntegerField(default=1)
+    done_date = models.DateTimeField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+
+    def __str__(self):
+        return self.title
+    
+
+class Habit(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    difficulty = models.SmallIntegerField(default=1)
+    times_per_week = models.SmallIntegerField(default=7)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class HabitInstance(models.Model):
+    date = models.DateField()
+    isDone = models.BooleanField(default=False)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='habit_instances')
+
+    def __str__(self):
+        return self.habit.title + ' - ' + str(self.date)
+    
+
+class WeeklyGoal(models.Model):
+    tasks = models.ManyToManyField(Task, related_name='weekly_goals')
+    subtasks = models.ManyToManyField(Subtask, related_name='weekly_goals')
+    first_day = models.DateField()
+    last_day = models.DateField()
+
+    def __str__(self):
+        return 'Weekly Goal: ' + str(self.first_day) + ' - ' + str(self.last_day)
