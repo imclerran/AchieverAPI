3ea3e38f975f97476afdba06d2aea425b4bb145c
Ian McLerran <imclerran@protonmail.com>
2024-04-15 17:19:35 -0500

    Add Viewsets for Task, Subtask, Habit, HabitInstance, and WeeklyGoal models

--- AchieverAPI/views.py
+++ AchieverAPI/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+class SubtaskViewSet(viewsets.ModelViewSet):
+    serializer_class = SubtaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return super().get_queryset().filter(task__user=self.request.user)
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+class HabitInstanceViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitInstanceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return super().get_queryset().filter(habit__user=self.request.user)
+
+class WeeklyGoalViewSet(viewsets.ModelViewSet):
+    serializer_class = WeeklyGoalSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().order_by('-first_day')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
